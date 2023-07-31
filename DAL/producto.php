46634c05c4a645da4a4d9da1c1e986c0447a37be
Xpronoob <kevin_03_puris@hotmail.com>
2023-07-31 13:06:00 -0600

    delete/update

--- DAL/producto.php
+++ DAL/producto.php
@@ -50,6 +50,18 @@ function readProducto()
             echo "Id: " . $row["id_producto"] . "<br>";
             echo "Descripcion: " . $row["descripcion"] . "<br>";
             echo "Precio: " . $row["precio"] . "<br>";
+
+            // Botón para eliminar el producto
+            echo "<form action='' method='post'>";
+            echo "<input type='hidden' name='eliminar_producto' value='" . $row["id_producto"] . "'>";
+            echo "<button type='submit'>Eliminar</button>";
+            echo "</form>";
+
+            // Botón para editar el producto
+            echo "<form action='' method='post'>";
+            echo "<input type='hidden' name='editar_producto' value='" . $row["id_producto"] . "'>";
+            echo "<button type='submit'>Editar</button>";
+            echo "</form>";
         }
 
     } catch (\Throwable $th) {
@@ -59,4 +71,76 @@ function readProducto()
     }
 
     return $stmt;
+}
+
+
+function eliminarProducto($idProducto)
+{
+    $retorno = false;
+    try {
+        $conexion = Conecta();
+
+        // Consulta SQL para eliminar el producto por su ID
+        $stmt = $conexion->prepare("DELETE FROM productos WHERE id_producto = ?");
+        $stmt->bind_param("i", $idProducto);
+
+        if ($stmt->execute()) {
+            $retorno = true;
+        }
+    } catch (\Throwable $th) {
+        echo $th;
+        // Manejo de errores o bitácoras
+    } finally {
+        Desconecta($conexion);
+    }
+
+    return $retorno;
+}
+
+function obtenerProductoPorID($idProducto)
+{
+    $producto = null;
+    try {
+        $conexion = Conecta();
+
+        // Consulta SQL para obtener el producto por su ID
+        $stmt = $conexion->prepare("SELECT * FROM productos WHERE id_producto = ?");
+        $stmt->bind_param("i", $idProducto);
+
+        if ($stmt->execute()) {
+            $resultado = $stmt->get_result();
+            $producto = $resultado->fetch_assoc();
+        }
+    } catch (\Throwable $th) {
+        echo $th;
+        // Manejo de errores o bitácoras
+    } finally {
+        Desconecta($conexion);
+    }
+
+    return $producto;
+}
+
+function actualizarProducto($idProducto, $nuevaDescripcion, $nuevoPrecio)
+{
+    $retorno = false;
+    try {
+        $conexion = Conecta();
+
+        // Consulta SQL para actualizar el producto por su ID
+        $stmt = $conexion->prepare("UPDATE productos SET descripcion = ?, precio = ? WHERE id_producto = ?");
+        // El tipo de dato para el precio debe ser "d" (double)
+        $stmt->bind_param("sdi", $nuevaDescripcion, $nuevoPrecio, $idProducto);
+
+        if ($stmt->execute()) {
+            $retorno = true;
+        }
+    } catch (\Throwable $th) {
+        echo $th;
+        // Manejo de errores o bitácoras
+    } finally {
+        Desconecta($conexion);
+    }
+
+    return $retorno;
 }
--- formulario.php
+++ formulario.php
@@ -23,6 +23,56 @@
 
         <?php
         include("DAL/producto.php");
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["eliminar_producto"])) {
+            $idProductoEliminar = $_POST["eliminar_producto"];
+            if (eliminarProducto($idProductoEliminar)) {
+                echo "El producto con ID $idProductoEliminar ha sido eliminado exitosamente.";
+            } else {
+                echo "No se pudo eliminar el producto con ID $idProductoEliminar.";
+            }
+        }
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["editar_producto"])) {
+            $idProductoEditar = $_POST["editar_producto"];
+            $productoEditar = obtenerProductoPorID($idProductoEditar);
+
+            if ($productoEditar) {
+                // Mostrar el formulario para editar el producto con los valores actuales
+                echo "<form action='' method='post'>";
+                echo "<input type='hidden' name='id_producto' value='" . $productoEditar["id_producto"] . "'>";
+                echo "<div>";
+                echo "<label for='descripcion'>Descripcion:</label>";
+                echo "<input type='text' name='descripcion' id='descripcion' value='" . $productoEditar["descripcion"] . "' placeholder='Descripcion'>";
+                echo "</div>";
+                echo "<div>";
+                echo "<label for='precio'>Precio:</label>";
+                echo "<input type='number' name='precio' id='precio' value='" . $productoEditar["precio"] . "' placeholder='Precio'>";
+                echo "</div>";
+                echo "<button type='submit'>Guardar cambios</button>";
+                echo "</form>";
+            } else {
+                echo "No se pudo obtener el producto con ID $idProductoEditar para editar.";
+            }
+        }
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["id_producto"]) && isset($_POST["descripcion"]) && isset($_POST["precio"])) {
+            $idProductoEditar = $_POST["id_producto"];
+            $nuevaDescripcion = $_POST["descripcion"];
+            $nuevoPrecio = $_POST["precio"];
+
+            // Lógica para validar los datos editados si es necesario
+        
+            // Actualizar el producto en la base de datos
+            if (actualizarProducto($idProductoEditar, $nuevaDescripcion, $nuevoPrecio)) {
+                echo "El producto con ID $idProductoEditar ha sido actualizado exitosamente.";
+            } else {
+                echo "No se pudo actualizar el producto con ID $idProductoEditar.";
+            }
+        }
+
+
+
         readProducto();
         ?>
 
